7def8de174bc0e6f4c4b62d7c2da50d10ce95fe4
Gerardo Belot <gbelot2003@hotmail.com>
2024-10-24 10:45:32 -0600

    :construction: agregando versificacion de contactos

--- app/repositories/contact_repo.py
+++ app/repositories/contact_repo.py
@@ -1,5 +1,5 @@
 # app/repositories/contact_repo.py
-from extensions import db
+from app.extensions import db
 from app.models.contact_model import Contact as Contacto
 
 class ContactRepo:
---
+++ app/services/action_handler_service.py
@@ -0,0 +1,14 @@
+# app/services/action_handler_service.py
+
+from app.actions.verify_contact_action import VerifyContactAction
+
+
+class ActionHandleService:  
+    def __init__(self, user_id, prompt):
+        self.user_id = user_id
+        self.prompt = prompt
+        self.messages = []
+    
+    def handle_actions(self):
+        # Verificar si el usuario tiene un número de teléfono en la base de datos
+        contacto = VerifyContactAction().verificar_contacto(self.user_id)        
