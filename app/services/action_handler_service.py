e48e9428486f2a5a2506d968b4759fa1faf96595
Gerardo Belot <gbelot2003@hotmail.com>
2024-10-24 10:58:18 -0600

    :construction: recuerda conversacion en db

--- app/services/action_handler_service.py
+++ app/services/action_handler_service.py
@@ -2,6 +2,7 @@
 
 from app.actions.name_action import NameAction
 from app.actions.verify_contact_action import VerifyContactAction
+from app.services.conversation_history_service import ConversationHistoryAction
 
 
 class ActionHandleService:  
@@ -13,6 +14,11 @@ class ActionHandleService:
     def handle_actions(self):
         # Verificar si el usuario tiene un número de teléfono en la base de datos
         contacto = VerifyContactAction().verificar_contacto(self.user_id)        
+
+        # Buscar historial de conversación
+        conversation_history_action = ConversationHistoryAction()
+        chat_history_messages = conversation_history_action.compilar_conversacion(self.user_id)
+        self.messages.extend(chat_history_messages)
 
         # Procesar el nombre del contacto
         name_action = NameAction(contacto, self.prompt)
